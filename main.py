f7c27d5f61828b76683c8f0ba35661c0bb38cd6f
statelinecat <statelinecat@gmail.com>
2024-08-23 03:12:35 +0300

    add image

--- main.py
+++ main.py
@@ -1,25 +1,21 @@
 from flask import Flask, render_template
 
+# Создаем экземпляр приложения Flask
 app = Flask(__name__)
 
+# Определяем маршруты для страниц
 @app.route('/')
-
 def index():
-
     return render_template('index.html')
 
-@app.route('/about')
-
+@app.route('/blog')
 def blog():
+    return render_template('blog.html')
 
-    return render_template('about.html')
-
-@app.route('/work')
-
+@app.route('/contacts')
 def contacts():
+    return render_template('contacts.html')
 
-    return render_template('work.html')
-
+# Запуск приложения
 if __name__ == '__main__':
-
     app.run(debug=True)
